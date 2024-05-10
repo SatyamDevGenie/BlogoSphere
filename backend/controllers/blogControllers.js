8368a464eb63e397724b31017935543271cf787f
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-05-10 22:32:26 +0530

    Create Blog Backend Functionality Done

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -1,3 +1,41 @@
+// import asyncHandler from "express-async-handler";
+
+// import Blog from "../models/blogModel.js";
+
+// /**
+//  * @desc		Get all blogs
+//  * @route		GET /api/blogs
+//  * @access	public
+//  */
+
+// const getBlogs = asyncHandler(async (req, res) => {
+//   const blogs = await Blog.find({});
+//   res.json(blogs);
+// });
+
+// /**
+//  * @desc		Get single blog
+//  * @route		GET /api/blogs/:id
+//  * @access	public
+//  */
+
+// const getBlogById = asyncHandler(async (req, res) => {
+//   try {
+//     const blog = await Blog.findById(req.params.id);
+
+//     if (blog) {
+//       res.json(blog);
+//     } else {
+//       res.status(404).json({ message: "Blog not found" });
+//     }
+//   } catch (err) {
+//     console.log(err.message);
+//     res.json(err);
+//   }
+// });
+
+// export { getBlogById, getBlogs };
+
 import asyncHandler from "express-async-handler";
 
 import Blog from "../models/blogModel.js";
@@ -34,4 +72,26 @@ const getBlogById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getBlogById, getBlogs };
+/**
+ * @desc		Create a product
+ * @route		POST /api/blogs/postBlog
+ * @access	private/admin
+ */
+const createBlog = asyncHandler(async (req, res) => {
+  const { title, content, image } = req.body;
+  try {
+    const newBlog = new Blog({
+      title,
+      content,
+      image,
+      user: req.user._id,
+    });
+    const createdBlog = await newBlog.save();
+    res.status(201).json(createdBlog);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+export { createBlog, getBlogById, getBlogs };
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getBlogById, getBlogs } from "../controllers/blogControllers.js";
+import {
+  createBlog,
+  getBlogById,
+  getBlogs,
+} from "../controllers/blogControllers.js";
+import { protect } from "../middlwares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(getBlogs);
+router.route("/").get(getBlogs).post(protect, createBlog);
 router.route("/:id").get(getBlogById);
 
 export default router;
