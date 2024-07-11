835db1610d287dae6308f0fcdf585b4343a99875
Your Name <your.email@example.com>
2024-07-12 05:22:37 +0530

    add like button

--- frontend/src/components/BlogCard.js
+++ frontend/src/components/BlogCard.js
@@ -1,3 +1,79 @@
+// import {
+//   Box,
+//   Flex,
+//   Heading,
+//   IconButton,
+//   Image,
+//   Link,
+//   Text,
+//   VStack,
+// } from "@chakra-ui/react";
+// import { IoEyeOutline } from "react-icons/io5";
+// import { useSelector } from "react-redux";
+// import { Link as RouterLink } from "react-router-dom";
+
+// const BlogCard = ({ blog }) => {
+//   const getRandomInt = (min, max) => {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   };
+
+//   const randomNumber = getRandomInt(1, 50);
+
+//   const userLogin = useSelector((state) => state.userLogin);
+//   const { userInfo } = userLogin;
+
+//   return (
+//     <Link
+//       as={RouterLink}
+//       to={`/blogs/${blog._id}`}
+//       _hover={{
+//         textDecoration: "none",
+//       }}
+//     >
+//       <Box
+//         borderRadius="md"
+//         overflow="hidden"
+//         bgColor="white"
+//         boxShadow="md"
+//         _hover={{
+//           boxShadow: "xl",
+//           transform: "translateY(-5px)",
+//           transition: "all 0.3s ease-in-out",
+//         }}
+//         transition="all 0.3s ease-in-out"
+//       >
+//         <Image
+//           src={blog.image}
+//           alt={blog.title}
+//           w="full"
+//           h="200px"
+//           objectFit="cover"
+//         />
+//         <VStack spacing={4} p={4} align="stretch">
+//           <Heading as="h4" fontSize="lg" fontFamily="sans-serif">
+//             {blog.title}
+//           </Heading>
+//           <Flex justifyContent="space-between" alignItems="center">
+//             <Flex alignItems="center" gap={2}>
+//               <IconButton
+//                 aria-label="View"
+//                 icon={<IoEyeOutline />}
+//                 size="sm"
+//                 variant="ghost"
+//               />
+//               <Text fontSize="sm">{randomNumber}</Text>
+//             </Flex>
+//           </Flex>
+//         </VStack>
+//       </Box>
+//     </Link>
+//   );
+// };
+
+// export default BlogCard;
+
 import {
   Box,
   Flex,
@@ -8,7 +84,7 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { IoEyeOutline } from "react-icons/io5";
+import { IoEyeOutline, IoHeartOutline } from "react-icons/io5";
 import { useSelector } from "react-redux";
 import { Link as RouterLink } from "react-router-dom";
 
@@ -64,6 +140,15 @@ const BlogCard = ({ blog }) => {
                 variant="ghost"
               />
               <Text fontSize="sm">{randomNumber}</Text>
+            </Flex>
+            <Flex alignItems="center" gap={2}>
+              <IconButton
+                aria-label="Like"
+                icon={<IoHeartOutline />}
+                size="sm"
+                variant="ghost"
+              />
+              <Text fontSize="sm">Like</Text>
             </Flex>
           </Flex>
         </VStack>
