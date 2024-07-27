ba0ebff7425012327e6778495007788e2d8852b7
Your Name <your.email@example.com>
2024-07-27 15:11:51 +0530

    add tailwind in register page

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,290 +1,3 @@
-// import {
-//   Button,
-//   Flex,
-//   FormControl,
-//   FormLabel,
-//   Heading,
-//   Input,
-//   Link,
-//   Spacer,
-//   Text,
-// } from "@chakra-ui/react";
-// import { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import {
-//   Link as RouterLink,
-//   useNavigate,
-//   useSearchParams,
-// } from "react-router-dom";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css"; // Import CSS for styling
-// import { register } from "../actions/userActions";
-// import FormContainer from "../components/FormContainer";
-// import Message from "../components/Message";
-
-// const RegisterScreen = () => {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   let [searchParams] = useSearchParams();
-//   let redirect = searchParams.get("redirect") || "/";
-
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [confirmPassword, setConfirmPassword] = useState("");
-//   const [message, setMessage] = useState(null);
-
-//   const userRegister = useSelector((state) => state.userRegister);
-//   const { loading, error, userInfo } = userRegister;
-
-//   useEffect(() => {
-//     if (userInfo) {
-//       navigate(redirect);
-//     }
-//   }, [userInfo, redirect, navigate]);
-
-//   const submitHandler = (e) => {
-//     e.preventDefault();
-//     if (password !== confirmPassword) {
-//       toast.error("Passwords do not match");
-//     } else {
-//       dispatch(register(name, email, password));
-//     }
-//   };
-
-//   return (
-//     <Flex w="full" alignItems="center" justifyContent="center" py="5" mt="5">
-//       <FormContainer>
-//         <Heading as="h1" mb="8" fontSize="4xl">
-//           Register
-//         </Heading>
-
-//         {error && <Message type="error">{error}</Message>}
-//         {message && <Message type="error">{message}</Message>}
-
-//         <form onSubmit={submitHandler}>
-//           <FormControl id="name">
-//             <FormLabel htmlFor="name">Your Name</FormLabel>
-//             <Input
-//               id="name"
-//               type="text"
-//               placeholder="Your full name"
-//               value={name}
-//               onChange={(e) => setName(e.target.value)}
-//             />
-//           </FormControl>
-
-//           <Spacer h="3" />
-
-//           <FormControl id="email">
-//             <FormLabel htmlFor="email">Email address</FormLabel>
-//             <Input
-//               id="email"
-//               type="email"
-//               placeholder="username@domain.com"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//           </FormControl>
-
-//           <Spacer h="3" />
-
-//           <FormControl id="password">
-//             <FormLabel htmlFor="password">Password</FormLabel>
-//             <Input
-//               id="password"
-//               type="password"
-//               placeholder="************"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//           </FormControl>
-
-//           <Spacer h="3" />
-
-//           <FormControl id="confirmPassword">
-//             <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
-//             <Input
-//               id="confirmPassword"
-//               type="password"
-//               placeholder="************"
-//               value={confirmPassword}
-//               onChange={(e) => setConfirmPassword(e.target.value)}
-//             />
-//           </FormControl>
-
-//           <Button type="submit" colorScheme="teal" mt="4" isLoading={loading}>
-//             Register
-//           </Button>
-//         </form>
-
-//         <Flex pt="10">
-//           <Text fontWeight="semibold">
-//             Already a Customer?{" "}
-//             <Link as={RouterLink} to="/login">
-//               Click here to login
-//             </Link>
-//           </Text>
-//         </Flex>
-//       </FormContainer>
-//       <ToastContainer /> {/* Render the ToastContainer component */}
-//     </Flex>
-//   );
-// };
-
-// export default RegisterScreen;
-
-// import {
-//   Button,
-//   Flex,
-//   FormControl,
-//   FormLabel,
-//   Heading,
-//   Input,
-//   Link,
-//   Spacer,
-//   Text,
-// } from "@chakra-ui/react";
-// import { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import {
-//   Link as RouterLink,
-//   useNavigate,
-//   useSearchParams,
-// } from "react-router-dom";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css"; // Import CSS for styling
-
-// import { register } from "../actions/userActions";
-// import FormContainer from "../components/FormContainer";
-// import Message from "../components/Message";
-
-// const RegisterScreen = () => {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const [searchParams] = useSearchParams();
-//   let redirect = searchParams.get("redirect") || "/";
-
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [confirmPassword, setConfirmPassword] = useState("");
-//   const [message, setMessage] = useState("");
-
-//   const userRegister = useSelector((state) => state.userRegister);
-//   const { loading, error, userInfo } = userRegister;
-
-//   useEffect(() => {
-//     if (userInfo) {
-//       navigate(redirect);
-//     }
-//   }, [navigate, userInfo, redirect]);
-
-//   const submitHandler = (e) => {
-//     e.preventDefault();
-//     if (password !== confirmPassword) {
-//       toast.error("Passwords do not match");
-//     } else {
-//       dispatch(register(name, email, password));
-//       toast.success(`${userInfo.name} registered successfully!`);
-//     }
-//   };
-
-//   return (
-//     <Flex w="full" alignItems="center" justifyContent="center" py="5">
-//       <FormContainer>
-//         <Heading as="h1" mb="8" fontSize="3xl">
-//           Register
-//         </Heading>
-
-//         {error && <Message type="error">{error}</Message>}
-//         {message && <Message type="error">{message}</Message>}
-
-//         <form onSubmit={submitHandler}>
-//           <FormControl id="name">
-//             <FormLabel htmlFor="name">Your Name</FormLabel>
-//             <Input
-//               id="name"
-//               type="text"
-//               placeholder="Your full name"
-//               value={name}
-//               onChange={(e) => setName(e.target.value)}
-//             />
-//           </FormControl>
-
-//           <Spacer h="3" />
-
-//           <FormControl id="email">
-//             <FormLabel htmlFor="email">Email address</FormLabel>
-//             <Input
-//               id="email"
-//               type="email"
-//               placeholder="username@domain.com"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//           </FormControl>
-
-//           <Spacer h="3" />
-
-//           <FormControl id="password">
-//             <FormLabel htmlFor="password">Password</FormLabel>
-//             <Input
-//               id="password"
-//               type="password"
-//               placeholder="************"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//           </FormControl>
-
-//           <Spacer h="3" />
-
-//           <FormControl id="confirmPassword">
-//             <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
-//             <Input
-//               id="confirmPassword"
-//               type="password"
-//               placeholder="************"
-//               value={confirmPassword}
-//               onChange={(e) => setConfirmPassword(e.target.value)}
-//             />
-//           </FormControl>
-
-//           <Button type="submit" colorScheme="teal" mt="4" isLoading={loading}>
-//             Register
-//           </Button>
-//         </form>
-
-//         <Flex pt="10">
-//           <Text fontWeight="semibold">
-//             Already a Customer?{" "}
-//             <Link as={RouterLink} to="/login">
-//               Click here to login
-//             </Link>
-//           </Text>
-//         </Flex>
-//       </FormContainer>
-//       <ToastContainer />
-//     </Flex>
-//   );
-// };
-
-// export default RegisterScreen;
-
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Link,
-  Spacer,
-  Text,
-} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -296,7 +9,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // Import CSS for styling
 
 import { register } from "../actions/userActions";
-import FormContainer from "../components/FormContainer";
 import Message from "../components/Message";
 
 const RegisterScreen = () => {
@@ -332,82 +44,105 @@ const RegisterScreen = () => {
   };
 
   return (
-    <Flex w="full" alignItems="center" justifyContent="center" py="5">
-      <FormContainer>
-        <Heading as="h1" mb="8" fontSize="3xl">
-          Register
-        </Heading>
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-lg">
+        <h1 className="text-3xl font-bold text-center mb-8">Register</h1>
 
         {error && <Message type="error">{error}</Message>}
         {message && <Message type="error">{message}</Message>}
 
-        <form onSubmit={submitHandler}>
-          <FormControl id="name">
-            <FormLabel htmlFor="name">Your Name</FormLabel>
-            <Input
+        <form onSubmit={submitHandler} className="space-y-4">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Your Name
+            </label>
+            <input
               id="name"
               type="text"
               placeholder="Your full name"
               value={name}
               onChange={(e) => setName(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
             />
-          </FormControl>
+          </div>
 
-          <Spacer h="3" />
-
-          <FormControl id="email">
-            <FormLabel htmlFor="email">Email address</FormLabel>
-            <Input
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <input
               id="email"
               type="email"
               placeholder="username@domain.com"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
             />
-          </FormControl>
+          </div>
 
-          <Spacer h="3" />
-
-          <FormControl id="password">
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
               id="password"
               type="password"
               placeholder="************"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
             />
-          </FormControl>
+          </div>
 
-          <Spacer h="3" />
-
-          <FormControl id="confirmPassword">
-            <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
-            <Input
+          <div>
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm Password
+            </label>
+            <input
               id="confirmPassword"
               type="password"
               placeholder="************"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
             />
-          </FormControl>
+          </div>
 
-          <Button type="submit" colorScheme="teal" mt="4" isLoading={loading}>
-            Register
-          </Button>
+          <button
+            type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Register"}
+          </button>
         </form>
 
-        <Flex pt="10">
-          <Text fontWeight="semibold">
-            Already have an account ?{" "}
-            <Link as={RouterLink} to="/login">
+        <div className="text-center pt-6">
+          <p className="text-sm font-semibold">
+            Already have an account?{" "}
+            <RouterLink
+              to="/login"
+              className="text-teal-600 hover:text-teal-500"
+            >
               Click here to login
-            </Link>
-          </Text>
-        </Flex>
-      </FormContainer>
+            </RouterLink>
+          </p>
+        </div>
+      </div>
       <ToastContainer />
-    </Flex>
+    </div>
   );
 };
 
